b08ba6fcfd4bfded0f084c259511b0dca7a5c2d2
sudharsan-ak <sudharsanak1010@gmail.com>
2026-06-03 18:38:30 -0500

    Renamed Week 1 folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Day01Setup from './week1/day01-setup/Day01Setup'
-import Day02ComponentsJSX from './week1/day02-components-jsx/Day02ComponentsJSX'
-import Day03Props from './week1/day03-props/Day03Props'
-import Day04TypeScriptProps from './week1/day04-typescript-props/Day04TypeScriptProps'
-import Day05StateUseState from './week1/day05-state-usestate/Day05StateUseState'
-import Day06EventHandling from './week1/day06-event-handling/Day06EventHandling'
-import Day07ConditionalListRendering from './week1/day07-conditional-list-rendering/Day07ConditionalListRendering'
+import Day01Setup from './Week 1/day01-setup/Day01Setup'
+import Day02ComponentsJSX from './Week 1/day02-components-jsx/Day02ComponentsJSX'
+import Day03Props from './Week 1/day03-props/Day03Props'
+import Day04TypeScriptProps from './Week 1/day04-typescript-props/Day04TypeScriptProps'
+import Day05StateUseState from './Week 1/day05-state-usestate/Day05StateUseState'
+import Day06EventHandling from './Week 1/day06-event-handling/Day06EventHandling'
+import Day07ConditionalListRendering from './Week 1/day07-conditional-list-rendering/Day07ConditionalListRendering'
 
 function App() {
   return(
